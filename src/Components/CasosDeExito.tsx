94168c7d635c2d9b3842187d108de47864a2401b
Facundo <facupadilla19904@gmail.com>
2024-07-30 11:07:06 -0300

    espaciado

--- src/Components/CasosDeExito.tsx
+++ src/Components/CasosDeExito.tsx
@@ -15,43 +15,56 @@ const CasosDeExito = () => {
         </h1>
       </div>
       <div className="lg:flex lg:flex-wrap   my-10">
-        <CardProject
-          title="Bodega Norton"
-          text="Estrategia de Redes sociales
+        <div className="lg:w-1/3">
+          <CardProject
+            title="Bodega Norton"
+            text="Estrategia de Redes sociales
 "
-          img="/portfolio1.jpg"
-          link="https://www.behance.net/gallery/148663751/Bodega-Norton-Social-media"
-        />
-        <CardProject
-          title="Chicco"
-          text="Un bebé lo cambia todo | Campaña publicitaria"
-          img="/portfolio2.JPG"
-          link="https://www.behance.net/gallery/147608179/Chicco-Campaign"
-        />
-        <CardProject
-          title="Allergan"
-          text=" Go for it | Campaña publicitaria"
-          img="/portfolio3.JPG"
-          link="https://www.behance.net/gallery/147506437/Allergan-Campaign"
-        />
-        <CardProject
-          title="La Malbequería"
-          text="Estrategia de Redes sociales"
-          img="/portfolio4.JPG"
-          link="https://www.behance.net/gallery/147510987/La-Malbequeria-Social-media"
-        />
-        <CardProject
-          title="Lo De Jesús"
-          text=" 66 fuegos | Campaña publicitaria"
-          img="/portfolio5.JPG"
-          link="https://www.behance.net/gallery/148733719/Lo-De-Jesus-Campaign"
-        />
-        <CardProject
-          title="Coolsculpting"
-          text="Sentite de 10, sentite Cool sculpting | Campaña publicitaria"
-          img="/portfolio6.JPG"
-          link="https://www.behance.net/gallery/147586873/Coolsculpting-Campaign"
-        />
+            img="/portfolio1.jpg"
+            link="https://www.behance.net/gallery/148663751/Bodega-Norton-Social-media"
+          />
+        </div>
+        <div className="lg:w-1/3">
+          <CardProject
+            title="Chicco"
+            text="Un bebé lo cambia todo | Campaña publicitaria"
+            img="/portfolio2.JPG"
+            link="https://www.behance.net/gallery/147608179/Chicco-Campaign"
+          />
+        </div>
+        <div className="lg:w-1/3">
+          <CardProject
+            title="Allergan"
+            text=" Go for it | Campaña publicitaria"
+            img="/portfolio3.JPG"
+            link="https://www.behance.net/gallery/147506437/Allergan-Campaign"
+          />
+        </div>
+        <div className="lg:w-1/3">
+          <CardProject
+            title="La Malbequería"
+            text="Estrategia de Redes sociales"
+            img="/portfolio4.JPG"
+            link="https://www.behance.net/gallery/147510987/La-Malbequeria-Social-media"
+          />
+        </div>
+        <div className="lg:w-1/3">
+          {" "}
+          <CardProject
+            title="Lo De Jesús"
+            text=" 66 fuegos | Campaña publicitaria"
+            img="/portfolio5.JPG"
+            link="https://www.behance.net/gallery/148733719/Lo-De-Jesus-Campaign"
+          />
+        </div>
+        <div className="lg:w-1/3">
+          <CardProject
+            title="Coolsculpting"
+            text="Sentite de 10, sentite Cool sculpting | Campaña publicitaria"
+            img="/portfolio6.JPG"
+            link="https://www.behance.net/gallery/147586873/Coolsculpting-Campaign"
+          />
+        </div>
       </div>
     </motion.div>
   );
