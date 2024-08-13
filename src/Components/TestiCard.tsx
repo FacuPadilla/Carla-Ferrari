86d012d88cc6f8af2a92afd63ce2f4c1119bd1af
Facundo <facupadilla19904@gmail.com>
2024-08-13 20:59:20 -0300

    bg

--- src/Components/TestiCard.tsx
+++ src/Components/TestiCard.tsx
@@ -15,7 +15,7 @@ export const TestiCard = ({
         href="https://www.linkedin.com/in/carla-ferrari-0b234220a/details/recommendations/?detailScreenTabIndex=0
       "
       >
-        <div className="bg-secondary h-[540px]  w-[500px] md:px-0 px-12 hover:scale-105 transform transition-all">
+        <div className="bg-transparent h-[540px]  w-[500px] md:px-0 px-12 hover:scale-105 transform transition-all">
           <div className="flex justify-between py-5 px-10">
             <h1 className="font-bold text-lg">{title}</h1>
             <h2 className="text-xs">{subtitle}</h2>
